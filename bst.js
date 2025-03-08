1921ded91c22f3e2f2698d21f5f3209db6a74822
Sven Dugojevic <sdugoje@gmail.com>
2025-03-07 22:06:14 -0800

    Down to last item to add

--- bst.js
+++ bst.js
@@ -3,6 +3,7 @@ class Node{
     this.data=data
     this.right=null
     this.left=null
+    this.parent = null
 }
 }
 
@@ -11,15 +12,17 @@ class Tree{
         const sortedArr = this.simpleSort(arr)
         const start = 0
         const end = sortedArr.length - 1
-        this.root = this.buildTree(sortedArr, start, end)
+        const parent = null;
+        this.root = this.buildTree(sortedArr, start, end, parent)
     }
 
-    buildTree(arr, start, end){
+    buildTree(arr, start, end, parent=null){
         if (start > end) return null;
         const mid = start + Math.floor((end - start)/2)
         const root = new Node(arr[mid])
-        root.left = this.buildTree(arr, start, mid-1)
-        root.right = this.buildTree(arr, mid+1, end)
+        root.parent = parent
+        root.left = this.buildTree(arr, start, mid-1, root)
+        root.right = this.buildTree(arr, mid+1, end, root)
 
         return root
     }
@@ -41,6 +44,94 @@ class Tree{
     }
     return root
 }
+    levelOrder(callback){
+        if (!callback){
+            throw new Error('missing callback!')
+        }
+        let queue = [this.root]
+        
+        while (queue.length > 0){
+            let node = queue.shift()
+            callback(node)
+            if (node.left) queue.push(node.left)
+            if (node.right) queue.push(node.right)
+        
+        }
+    }
+    recursiveLeveLOrder(callback, queue = [this.root]){
+        if (!callback){
+            throw new Error('missing callback!')
+        }
+        if (queue.length === 0){
+            return
+        }
+        const node = queue.shift()
+        callback(node)
+        if (node.left) queue.push(node.left);
+        if (node.right) queue.push(node.right);
+
+        this.recursiveLeveLOrder(callback, queue)
+    }
+    inOrder(callback, node = this.root){
+        if (!callback){
+            throw new Error('missing callback!')
+        }
+        if (node == null){
+            return
+        }
+        this.inOrder(callback, node.left)
+        callback(node) 
+        this.inOrder(callback, node.right)
+    }
+    preOrder(callback, node = this.root){
+        if (!callback){
+            throw new Error('missing callback!')
+        }
+        if (node == null){
+            return
+        }
+        callback(node) 
+        this.preOrder(callback, node.left)
+        this.preOrder(callback, node.right)
+    }
+    postOrder(callback, node = this.root){
+        if (!callback){
+            throw new Error('missing callback!')
+        }
+        if (node == null){
+            return
+        }
+        this.postOrder(callback, node.left)
+        this.postOrder(callback, node.right)
+        callback(node) 
+    }    
+    height(node){
+        if (node==null){
+            return -1
+        }
+        let leftHeight = this.height(node.left)
+        let rightHeight = this.height(node.right)
+        return 1 + Math.max(leftHeight, rightHeight)
+    }
+    find(value, node = this.root){
+        if (node === null || node.data === value ){
+            return node
+        }
+        if (value < node.data){
+            return this.find(value, node.left)
+        } else if (value > node.data){
+            return this.find(value, node.right)
+        }
+    }
+    depth(node){
+        if (node == null) {
+            return -1; 
+        }
+        if (node == this.root){
+            return 0
+        }
+       return 1 + this.depth(node.parent)
+    }
 }
 const prettyPrint = (node, prefix = "", isLeft = true) => {
     if (node === null) {
@@ -60,7 +151,4 @@ const testArray = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
 
 const testTree = new Tree(testArray)
 prettyPrint(testTree.root)
-
-testTree.insert(testTree.root, 69)
-prettyPrint(testTree.root)
-
+console.log(testTree.depth(testTree.find(7)))
